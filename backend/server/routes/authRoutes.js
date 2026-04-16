1644f216545b9692baa3125931b10b9edde4a5c2
bz0074 <128610052+bz0074@users.noreply.github.com>
2026-04-16 13:59:45 -0500

    Update authRoutes.js

--- backend/server/routes/authRoutes.js
+++ backend/server/routes/authRoutes.js
@@ -1,1 +1,11 @@
+import express from 'express'
+import { getUserProfile, loginUser, signupUser } from '../controllers/authController.js'
+import authMiddleware from '../middleware/authMiddleware.js'
 
+const router = express.Router()
+
+router.post('/signup', signupUser)
+router.post('/login', loginUser)
+router.get('/profile', authMiddleware, getUserProfile)
+
+export default router
