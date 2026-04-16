3aa72f0ff9f710d811939ca25184e8925c9fa458
bz0074 <128610052+bz0074@users.noreply.github.com>
2026-04-16 12:53:17 -0500

    Update orderAPI.js

--- src/features/orders/orderAPI.js
+++ src/features/orders/orderAPI.js
@@ -1,1 +1,16 @@
+import api from '../../api/axios'
 
+export const createOrderAPI = async (orderData) => {
+  const response = await api.post('/orders', orderData)
+  return response.data
+}
+
+export const getMyOrdersAPI = async () => {
+  const response = await api.get('/orders/my-orders')
+  return response.data
+}
+
+export const getAllOrdersAPI = async () => {
+  const response = await api.get('/orders')
+  return response.data
+}
