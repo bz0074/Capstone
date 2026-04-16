23e1a8001ded42a7d05587bdd8a4773f8bbff100
bz0074 <128610052+bz0074@users.noreply.github.com>
2026-04-16 13:00:27 -0500

    Update NotFound.jsx

--- src/pages/NotFound.jsx
+++ src/pages/NotFound.jsx
@@ -1,1 +1,12 @@
+const NotFound = () => {
+  return (
+    <div className="container-custom py-10">
+      <div className="card text-center">
+        <h1 className="text-3xl font-bold mb-4">404</h1>
+        <p>Page not found.</p>
+      </div>
+    </div>
+  )
+}
 
+export default NotFound
