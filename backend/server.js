b2dd0537876b6cc8e8237d861b1ebaa970e4a50e
bz0074 <128610052+bz0074@users.noreply.github.com>
2026-04-16 17:26:05 -0500

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -52,7 +52,7 @@ app.get('/api/orders', (req, res) => {
   res.json([])
 })
 
-const PORT = process.env.PORT || 5001
+const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`)
