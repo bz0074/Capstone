32f0c5567851bc196bab2ea00b930ebd2830c62d
bz0074 <128610052+bz0074@users.noreply.github.com>
2026-04-16 12:52:09 -0500

    Update cartAPI.js

--- src/features/cart/cartAPI.js
+++ src/features/cart/cartAPI.js
@@ -1,1 +1,11 @@
+import api from '../../api/axios'
 
+export const saveCartAPI = async (cartItems) => {
+  const response = await api.post('/cart', { items: cartItems })
+  return response.data
+}
+
+export const getCartAPI = async () => {
+  const response = await api.get('/cart')
+  return response.data
+}
