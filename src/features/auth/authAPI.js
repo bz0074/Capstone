10010370387f4f4dbba4770c1af4b218bb04be94
bz0074 <128610052+bz0074@users.noreply.github.com>
2026-04-16 12:50:05 -0500

    Update authAPI.js

--- src/features/auth/authAPI.js
+++ src/features/auth/authAPI.js
@@ -1,1 +1,11 @@
+import api from '../../api/axios'
 
+export const signupUserAPI = async (userData) => {
+  const response = await api.post('/auth/signup', userData)
+  return response.data
+}
+
+export const loginUserAPI = async (userData) => {
+  const response = await api.post('/auth/login', userData)
+  return response.data
+}
