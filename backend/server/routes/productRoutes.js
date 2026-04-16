0d37abdf732bab2e6837ac35ca490b9f81e9aeae
bz0074 <128610052+bz0074@users.noreply.github.com>
2026-04-16 14:00:02 -0500

    Update productRoutes.js

--- backend/server/routes/productRoutes.js
+++ backend/server/routes/productRoutes.js
@@ -1,1 +1,22 @@
+import express from 'express'
+import {
+  createProduct,
+  deleteProduct,
+  getProductById,
+  getProducts,
+  updateProduct,
+} from '../controllers/productController.js'
+import authMiddleware from '../middleware/authMiddleware.js'
+import adminMiddleware from '../middleware/adminMiddleware.js'
 
+const router = express.Router()
+
+router.route('/').get(getProducts).post(authMiddleware, adminMiddleware, createProduct)
+
+router
+  .route('/:id')
+  .get(getProductById)
+  .put(authMiddleware, adminMiddleware, updateProduct)
+  .delete(authMiddleware, adminMiddleware, deleteProduct)
+
+export default router
