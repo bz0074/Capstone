bcd0e8fd20c2ad0e4db57e2b3ac5eeb38549fc22
bz0074 <128610052+bz0074@users.noreply.github.com>
2026-04-16 12:51:24 -0500

    Update productAPI.js

--- src/features/products/productAPI.js
+++ src/features/products/productAPI.js
@@ -1,1 +1,21 @@
+import api from '../../api/axios'
 
+export const getProductsAPI = async () => {
+  const response = await api.get('/products')
+  return response.data
+}
+
+export const getProductByIdAPI = async (id) => {
+  const response = await api.get(`/products/${id}`)
+  return response.data
+}
+
+export const createProductAPI = async (productData) => {
+  const response = await api.post('/products', productData)
+  return response.data
+}
+
+export const deleteProductAPI = async (id) => {
+  const response = await api.delete(`/products/${id}`)
+  return response.data
+}
